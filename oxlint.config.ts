e9e38bf4b9de21d506847c1d91bc85a7a3c6cbff
Michon van Dooren <me@maienm.com>
2026-06-21 00:21:46 +0200

    Initialize oxlint config

---
+++ oxlint.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'oxlint';
+import airbnb from 'oxlint-config-presets/airbnb.json' with { type: 'json' };
+import tsStrict from 'oxlint-config-presets/@typescript-eslint/strict-type-checked.json' with { type: 'json' };
+
+export default defineConfig({
+	extends: [
+		airbnb,
+		tsStrict,
+	],
+	plugins: [
+		'typescript',
+		'unicorn',
+		'oxc',
+	],
+	categories: {
+		correctness: 'error',
+	},
+	rules: {
+	},
+	env: {
+		builtin: true,
+	},
+});
